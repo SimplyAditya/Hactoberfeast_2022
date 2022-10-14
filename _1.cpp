d2ba24a62541e786b93d1efcb1e77b4e0a01129c
HemantChaudhary11 <115036891+HemantChaudhary11@users.noreply.github.com>
2022-10-14 10:51:23 +0530

    3

---
+++ _1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Single line commant
+
+/*Multiline
+Commant*/
+
+int main()
+{
+    // Adding variables
+    int a = 6;
+    cout << "Hello, World\n"<< a; //cout <<"Hello World"<< endl<<a;
+    // \n /endl is used to change line
+    return 0;
+}
